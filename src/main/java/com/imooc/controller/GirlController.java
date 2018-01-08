78760f43d341a44d9893df2d3c9fa70821bbdafb
KingSea168 <qykingsea_168@163.com>
2018-01-08 09:44:41 +0800

    spring boot first version

---
+++ src/main/java/com/imooc/controller/GirlController.java
@@ -0,0 +1,121 @@
+package com.imooc.controller;
+
+import com.imooc.domain.Girl;
+import com.imooc.domain.Result;
+import com.imooc.repository.GirlRepository;
+import com.imooc.service.GirlService;
+import com.imooc.utils.ResultUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import java.awt.GridBagLayout;
+import java.util.List;
+
+/**
+ * Created by 廖师兄
+ * 2016-11-03 23:15
+ */
+@RestController
+public class GirlController {
+
+    private final static Logger logger = LoggerFactory.getLogger(GirlController.class);
+
+    @Autowired
+    private GirlRepository girlRepository;
+
+    @Autowired
+    private GirlService girlService;
+
+    /**
+     * 查询所有女生列表
+     * @return
+     */
+    @GetMapping(value = "/girls")
+    public List<Girl> girlList() {
+        logger.info("girlList");
+
+        return girlRepository.findAll();
+    }
+
+    /**
+     * 添加一个女生
+     * @return
+     * 成功：
+     * {
+     * 	"code":0,
+     * 	"msg":"成功",
+     * 	"data":{
+     * 		"id":20,
+     * 		"cupSize":"B",
+     * 		"age":25,
+     * 		"money":1.2
+     * 	}
+     * }
+     * 
+     * 失败：
+     * {
+     * 	"code":1,
+     * 	"msg":"金额必传",
+     * 	"data":null
+     * }
+     */
+    @PostMapping(value = "/girls")
+    public Result<Girl> girlAdd(@Valid Girl girl, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResultUtil.error(1, bindingResult.getFieldError().getDefaultMessage());
+        }
+
+        girl.setCupSize(girl.getCupSize());
+        girl.setAge(girl.getAge());
+
+        return ResultUtil.success(girlRepository.save(girl));
+    }
+
+    //查询一个女生
+    @GetMapping(value = "/girls/{id}")
+    public Girl girlFindOne(@PathVariable("id") Integer id) {
+        return girlRepository.findOne(id);
+    }
+
+    //更新
+    @PutMapping(value = "/girls/{id}")
+    public Girl girlUpdate(@PathVariable("id") Integer id,
+                           @RequestParam("cupSize") String cupSize,
+                           @RequestParam("age") Integer age,
+                           @RequestParam("money") Double money) {
+        Girl girl = new Girl();
+        girl.setId(id);
+        girl.setCupSize(cupSize);
+        girl.setAge(age);
+        girl.setMoney(money);
+
+        return girlRepository.save(girl);
+    }
+
+    //删除
+    @DeleteMapping(value = "/girls/{id}")
+    public void girlDelete(@PathVariable("id") Integer id) {
+        girlRepository.delete(id);
+    }
+
+    //通过年龄查询女生列表
+    @GetMapping(value = "/girls/age/{age}")
+    public List<Girl> girlListByAge(@PathVariable("age") Integer age) {
+        return girlRepository.findByAge(age);
+    }
+
+    @PostMapping(value = "/girls/two")
+    public void girlTwo() {
+        girlService.insertTwo();
+    }
+
+    @GetMapping(value = "girls/getAge/{id}")
+    public void getAge(@PathVariable("id") Integer id) throws Exception{
+        girlService.getAge(id);
+    }
+}
